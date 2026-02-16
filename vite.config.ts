6d5981ac36712143c76d7c284b8b41e493de3600
romez100 <romez100@gmail.com>
2026-02-16 15:26:53 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/romas-teach-profile/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
